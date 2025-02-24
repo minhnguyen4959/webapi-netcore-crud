3d4a84ec43b93598efc0e236701491da068d60a4
000259 <minh.nguyenquang@meiko.vn>
2025-02-24 18:00:47 +0700

    [Minh] - Init project web api user CRUD

---
+++ user-crud/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using user_crud.Models;
+
+namespace user_crud.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}

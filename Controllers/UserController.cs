01e4790d5bb82678e4ca8cac52128cfe39d48e8c
000259 <minh.nguyenquang@meiko.vn>
2025-02-25 12:22:18 +0700

    [Minh] - Convert to sln project

---
+++ Controllers/UserController.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Mvc;
+using user_crud.Data;
+using user_crud.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace user_crud.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public UserController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/user
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+		{
+			return await _context.Users.ToListAsync();
+		}
+
+		// GET: api/user/{id}
+		[HttpGet("{id}")]
+		public async Task<ActionResult<User>> GetUser(int id)
+		{
+			var user = await _context.Users.FindAsync(id);
+			if (user == null)
+				return NotFound();
+
+			return user;
+		}
+
+		// POST: api/user
+		[HttpPost]
+		public async Task<ActionResult<User>> CreateUser(User user)
+		{
+			_context.Users.Add(user);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+		}
+
+		// PUT: api/user/{id}
+		[HttpPut("{id}")]
+		public async Task<IActionResult> UpdateUser(int id, User user)
+		{
+			if (id != user.Id)
+				return BadRequest();
+
+			_context.Entry(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		// DELETE: api/user/{id}
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteUser(int id)
+		{
+			var user = await _context.Users.FindAsync(id);
+			if (user == null)
+				return NotFound();
+
+			_context.Users.Remove(user);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+	}
+}
